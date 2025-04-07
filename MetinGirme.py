ffde903dbadf49c1b7155842f8a43a05fcc5e0ce
meldanurcelik <d_mecelik@borusan.com>
2025-04-07 14:42:15 +0300

    Metin / yazı girme

---
+++ MetinGirme.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service("./chromedriver-win64/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.get("https://duckduckgo.com/")
+driver.maximize_window()
+aramakutusu = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "searchbox_input"))
+)
+aramakutusu.send_keys("selenium")
+aramakutusu.send_keys(Keys.ENTER)
