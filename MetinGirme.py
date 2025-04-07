d990812c098ece61dbc59f4ea813606a1deacfa8
meldanurcelik <d_mecelik@borusan.com>
2025-04-07 15:01:56 +0300

    Tıklama (Click)

--- MetinGirme.py
+++ MetinGirme.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 service = Service("./chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -13,4 +14,8 @@ aramakutusu = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "searchbox_input"))
 )
 aramakutusu.send_keys("selenium")
-aramakutusu.send_keys(Keys.ENTER)
+# aramakutusu.send_keys(Keys.ENTER)
+driver.find_element(By.CSS_SELECTOR, ".iconButton_button__A_Uiu.searchbox_searchButton__LxebD").click()
+driver.find_element(By.ID, "r1-2").click()
+
+time.sleep(10)
